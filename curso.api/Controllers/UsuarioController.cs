4eb21856adfe6b82cbf812e5676a05ccfa0371b0
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-07 22:38:45 -0300

    Login e verbo POST

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
